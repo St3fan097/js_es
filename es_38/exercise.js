21c6cebb83cb2a093b8d9932931cbb7504484711
St3fan097 <Ste97.esse@gmail.com>
2023-10-07 12:31:18 +0200

    exercise-38

---
+++ es_38/exercise.js
@@ -0,0 +1,26 @@
+const isLogged = true;
+
+function first(isLogged){
+    return new Promise((resolve, reject) => {
+    if (isLogged == true){
+        resolve(Math.random())
+    } else{
+        reject(new Error("Errore"))
+    }
+})
+}
+
+function second(num){
+    return new Promise((resolve, reject) => {
+    if (num > 0.5) {
+        resolve({ nome: "John", età: 24 })
+    } else {
+        reject(new Error("Errore"))
+    }
+})
+}
+
+first(isLogged)
+.then((num)=> second(num))
+.then((res)=>console.log(res))
+.catch((err)=> console.error(err));
