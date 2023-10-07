fc9743ceb410829ab275100e46cd34947f65bfa7
St3fan097 <Ste97.esse@gmail.com>
2023-10-07 11:10:03 +0200

    fix/exercise-34

--- es_34/exercise.js
+++ es_34/exercise.js
@@ -1,16 +1,15 @@
-function printAsyncName(callback,name) {
-    let count = 0;
+function printAsyncName(callback, name) {
     let id = setInterval(() => {
-        if (count === 0) {
-            console.log("Hello")
-        } else if (count === 1) {
-            console.log(name)
-        } else {
-            clearInterval(id);
-            callback(); //ho notato che se lo commento funziona ugualmente, come mai?
-        }
-        count++;
-    }, 1000);
-}
+        let sayHello = "Hello";
+        callback(sayHello);
+    },1000);
+    let id2 = setInterval(()=> {
+        console.log(name);
+    }, 2000)
+    setTimeout(() => {
+        clearInterval(id);
+    clearInterval(id2);
+}, 2000);
+    }
 
-printAsyncName(() => {}, "Steve");
+printAsyncName((any)=> console.log(any), "Steve");
